4a9fa03c474c0ae982444c8b94fe5ca0c5b2741f
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-28 04:14:34 -0800

    Create rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,41 @@
+#include "rectangle.hpp"
+
+Rectangle::Rectangle()
+{
+
+}
+
+Rectangle::~Rectangle()
+{
+
+}
+
+float Rectangle::x() const noexcept
+{
+    return shape.getPosition().x;
+}
+
+float Rectangle::y() const noexcept
+{
+    return shape.getPosition().y;
+}
+
+float Rectangle::left() const noexcept
+{
+    return x() - shape.getSize().x / 2.f;
+}
+
+float Rectangle::right() const noexcept
+{
+    return x() + shape.getSize().x / 2.f;
+}
+
+float Rectangle::top() const noexcept
+{
+    return y() - shape.getSize().y / 2.f;
+}
+
+float Rectangle::bottom() const noexcept
+{
+    return y() + shape.getSize().y / 2.f;
+}
