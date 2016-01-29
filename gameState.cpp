68f77e5e356e219f06215b2bad24d8f49b6d38b9
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-29 05:31:13 -0800

    Create gameState.cpp

---
+++ gameState.cpp
@@ -0,0 +1,36 @@
+#include "gameState.hpp"
+
+GameState::GameState()
+{
+
+}
+
+GameState::~GameState()
+{
+
+}
+
+void GameState::Pause()
+{
+
+}
+
+void GameState::Resume()
+{
+
+}
+
+void GameState::HandleEvents()
+{
+
+}
+
+void GameState::Update()
+{
+
+}
+
+void GameState::Draw()
+{
+
+}
