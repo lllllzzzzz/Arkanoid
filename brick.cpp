3600f8212835397a208c9a13315c5fd967b10721
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-28 23:26:03 -0800

    Bricks are now random colours

--- brick.cpp
+++ brick.cpp
@@ -1,10 +1,35 @@
 #include "Brick.hpp"
 
+#include <ctime>
+
 Brick::Brick(float posX, float posY)
 {
+    /*if (!texture.loadFromFile("data\\graphics\\brick_texture.png")) {
+    }
+
+    sprite.setTexture(texture);
+
+    sf::IntRect subRect;
+    subRect.left = 0;
+    subRect.top = 0;
+    subRect.width = 20;
+    subRect.height = 20;
+
+    shape.setTextureRect(subRect);
+    shape.setTexture(&texture);*/
+
     shape.setPosition(posX, posY);
     shape.setSize({brickWidth, brickHeight});
-    shape.setFillColor(Color::Red);
+
+    // Generate random colour for brick
+    //std::srand(std::time(0));
+
+    int r = std::rand() % 256;
+    int g = std::rand() % 256;
+    int b = std::rand() % 256;
+    Color color(r, g, b);
+    shape.setFillColor(color);
+
     shape.setOrigin(brickWidth / 2.f, brickHeight / 2.f);
     isDestroyed = false;
 }
