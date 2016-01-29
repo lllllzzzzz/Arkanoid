c32c99e5bd9880c7e6b6925badfddc57b4381583
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-29 05:32:43 -0800

    Create playState.cpp

---
+++ playState.cpp
@@ -0,0 +1,36 @@
+#include "playState.hpp"
+
+PlayState::PlayState()
+{
+
+}
+
+PlayState::~PlayState()
+{
+
+}
+
+void PlayState::Pause()
+{
+
+}
+
+void PlayState::Resume()
+{
+
+}
+
+void PlayState::HandleEvents()
+{
+
+}
+
+void PlayState::Update()
+{
+
+}
+
+void PlayState::Draw()
+{
+    
+}
