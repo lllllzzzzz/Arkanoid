9c614f9aa634d1a291da9a277b01ea34008759bf
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-01-30 00:42:04 -0800

    Removed redundant definition of base class methods

--- ball.cpp
+++ ball.cpp
@@ -1,10 +1,10 @@
 #include "ball.hpp"
 
-Ball::Ball(float posX, float posY, int radius)
+Ball::Ball(const float posX, const float posY, const float radius, sf::Color color)
 {
     shape.setPosition(posX, posY);
     shape.setRadius(radius);
-    shape.setFillColor(Color::White);
+    shape.setFillColor(color);
     shape.setOrigin(radius / 2, radius / 2);
 }
 
@@ -13,61 +13,31 @@ Ball::~Ball()
 
 }
 
-float Ball::x() const noexcept
-{
-    return shape.getPosition().x;
-}
-
-float Ball::y() const noexcept
-{
-    return shape.getPosition().y;
-}
-
-float Ball::left() const noexcept
-{
-    return x() - shape.getRadius();
-}
-
-float Ball::right() const noexcept
-{
-    return x() + shape.getRadius();
-}
-
-float Ball::top() const noexcept
-{
-    return y() - shape.getRadius();
-}
-
-float Ball::bottom() const noexcept
-{
-    return y() + shape.getRadius();
-}
-
-void Ball::setPos(int posX, int posY)
+void Ball::setPos(const float posX, const float posY)
 {
     shape.setPosition(posX, posY);
 }
 
-void Ball::setVelocity(int x, int y)
+void Ball::setVelocity(const int x, const int y)
 {
     velocity.x = x;
     velocity.y = y;
 }
 
-void Ball::update()
+void Ball::update(const int windowWidth, const int windowHeight)
 {
+    // Move ball at velocity and angle specified by vector
     shape.move(velocity);
 
-    // Keep the ball in horizontal bounds of the window
+    // Rebound ball off of sides of window
     if (left() < 0) {
         velocity.x = ballVelocity;
     } else if (right() > windowWidth) {
         velocity.x = -ballVelocity;
     }
-    // Keep the ball in vertical bounds of the window
+
+    // Rebound ball off of top of window
     if (top() < 0) {
         velocity.y = ballVelocity;
-    } else if (bottom() > windowHeight) {
-        velocity.y = -ballVelocity;
     }
 }
