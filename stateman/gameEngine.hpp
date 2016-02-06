53438c2829154f8cc8f501c9148e5440fd107b78
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-02-06 05:03:39 -0800

    Handling window events in GameEngine class

--- stateman/gameEngine.hpp
+++ stateman/gameEngine.hpp
@@ -5,36 +5,54 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-#include "..\states\gameState.hpp"
+#include "..\resourceman\resourceManager.hpp"
+//#include "..\states\gameState.hpp"
 
 class GameState;
 
-class GameEngine
-{
-public:
-    GameEngine(const int width, const int height, const int fps, bool fullscreen);
-    ~GameEngine();
+/*namespace Arkanoid
+{*/
+    class GameEngine
+    {
+    public:
+        GameEngine(const int width, const int height, const int fps, const std::string& windowTitle);
+        ~GameEngine();
 
-    void pushState(GameState* state);
-    void popState();
-    GameState* peekState();
-    void changeState(GameState* state);
+        void pushState(GameState* state);
+        void popState();
+        void changeState(GameState* state);
+        GameState* peekState();
 
-    void HandleEvents();
-    void Update();
-    void Draw();
+        void Run();
 
-    bool isRunning() const noexcept { return m_isRunning; }
-    void Quit() { m_isRunning = false; }
+        void HandleWindowEvents();
+        void HandleEvents();
+        void Update();
+        void Draw();
 
-    std::stack<GameState*> states;
-    sf::RenderWindow window;
+        bool isRunning() const noexcept;
+        void Quit();
 
-    int windowWidth;
-    int windowHeight;
+        bool isWindowMoving() const noexcept;
+        void moveWindow(const bool moveWindow);
 
-private:
-    bool m_isRunning;
-};
+        sf::RenderWindow& getWindow();
+        int getWindowWidth() const noexcept;
+        int getWindowHeight() const noexcept;
+        void setWindowSize(const int width, const int height);
+
+        ResourceManager resourceMan;
+
+    private:
+        sf::RenderWindow m_window;
+
+        std::stack<GameState*> states;
+
+        bool m_isRunning;
+        bool m_isWindowMoving;
+        int m_windowWidth;
+        int m_windowHeight;
+    };
+//}
 
 #endif // GAME_ENGINE_HPP
