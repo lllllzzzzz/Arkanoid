ae25b74a19e29df7b3338b4d042a79e2e69385b1
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-02-06 05:02:50 -0800

    Handling window events in GameEngine class

--- stateman/gameEngine.cpp
+++ stateman/gameEngine.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "gameEngine.hpp"
+#include "..\resourceman\resourceManager.hpp"
 #include "..\states\gameState.hpp"
 
-GameEngine::GameEngine(const int width, const int height, const int fps, bool fullscreen) :
-window(sf::VideoMode(width, height), "Arkanoid", fullscreen ? sf::Style::Fullscreen : sf::Style::Default)
+//using namespace Arkanoid;
+
+GameEngine::GameEngine(const int width, const int height, const int fps, const std::string& windowTitle) :
+    m_window({width, height}, windowTitle, sf::Style::Titlebar | sf::Style::Close),
+    m_windowWidth(width),
+    m_windowHeight(height),
+    m_isWindowMoving(false)
 {
-    this->windowWidth = width;
-    this->windowHeight = height;
-    window.setFramerateLimit(fps);
+    m_window.setFramerateLimit(fps);
 }
 
 GameEngine::~GameEngine()
@@ -22,12 +26,25 @@ void GameEngine::pushState(GameState* state)
     }
 
     states.push(state);
+
+    peekState()->Init(this);
 }
 
 void GameEngine::popState()
 {
     delete states.top();
     states.pop();
+}
+
+void GameEngine::changeState(GameState* state)
+{
+    if (!states.empty()) {
+        popState();
+    }
+
+    pushState(state);
+
+    peekState()->Init(this);
 }
 
 GameState* GameEngine::peekState()
@@ -39,13 +56,29 @@ GameState* GameEngine::peekState()
     return states.top();
 }
 
-void GameEngine::changeState(GameState* state)
+void GameEngine::Run()
 {
-    if (!states.empty()) {
-        popState();
-    }
+    HandleWindowEvents();
+    HandleEvents();
+    Update();
+    Draw();
+}
 
-    pushState(state);
+void GameEngine::HandleWindowEvents()
+{
+    // Poll window events
+    sf::Event event;
+    while (getWindow().pollEvent(event)) {
+        if (isWindowMoving()) {
+            std::cout << "left mouse button clicked\n";
+            getWindow().setPosition({sf::Mouse::getPosition().x - getWindowWidth() / 2, sf::Mouse::getPosition().y - getWindowHeight() / 2});
+        } if (event.type == sf::Event::MouseButtonPressed/* && event.MouseButton.Button == sf::Mouse::Left*/) {
+            moveWindow(true);
+        } if (event.type == sf::Event::MouseButtonReleased/* && event.MouseButton.Button == sf::Mouse::Left*/) {
+            moveWindow(false);
+            Quit();
+        }
+    }
 }
 
 void GameEngine::HandleEvents()
@@ -61,4 +94,45 @@ void GameEngine::Update()
 void GameEngine::Draw()
 {
     states.top()->Draw(this);
+}
+
+bool GameEngine::isRunning() const noexcept
+{
+    return m_isRunning;
+}
+
+void GameEngine::Quit()
+{
+    m_isRunning = false;
+}
+
+bool GameEngine::isWindowMoving() const noexcept
+{
+    return m_isWindowMoving;
+}
+
+void GameEngine::moveWindow(const bool moveWindow)
+{
+    m_isWindowMoving = moveWindow;
+}
+
+sf::RenderWindow& GameEngine::getWindow()
+{
+    return m_window;
+}
+
+int GameEngine::getWindowWidth() const noexcept
+{
+    return m_windowWidth;
+}
+
+int GameEngine::getWindowHeight() const noexcept
+{
+    return m_windowHeight;
+}
+
+void GameEngine::setWindowSize(const int width, const int height)
+{
+    m_windowWidth = width;
+    m_windowWidth = height;
 }
