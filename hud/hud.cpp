8bba63c2879e39b36abc4028a7332e8499100856
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-02-07 22:37:00 -0800

    HUD adapts to window size

--- hud/hud.cpp
+++ hud/hud.cpp
@@ -1,15 +1,25 @@
 #include "hud.hpp"
 
+#include <windows.h>
+
 const sf::Color Hud::scoreBackgroundColour = sf::Color::Black;
 const int Hud::scoreBackgroundWidth = 480;
 const int Hud::scoreBackgroundHeight = 30;
 
 Hud::Hud()
 {
-    if (!font.loadFromFile("data\\font\\PressStart2P.ttf")) {
-    }
-    if (!textureLives.loadFromFile("data\\graphics\\bat.png")) {
-    }
+
+}
+
+Hud::~Hud()
+{
+
+}
+
+void Hud::Init(GameEngine *game)
+{
+    textureLives = game->resourceMan.GetTexture("paddle.png");
+    font = game->resourceMan.GetFont("PressStart2P.ttf");
 
     text.setFont(font);
     text.setCharacterSize(14);
@@ -19,62 +29,56 @@ Hud::Hud()
     lives.setTexture(&textureLives);
     lives.setOrigin(50 / 2, 20 / 2);
 
-    scoreBackground.setSize({scoreBackgroundWidth, scoreBackgroundHeight});
+    scoreBackground.setSize({game->getWindowWidth(), scoreBackgroundHeight});
     scoreBackground.setFillColor(sf::Color::Black);
     scoreBackground.setOrigin(0, 0);
 }
 
-Hud::~Hud()
+void Hud::displayHud(GameEngine *game, int playerScore, int highScore, int numLives, int level, bool playerIsPlaying)
 {
-
-}
-
-void Hud::displayHud(sf::RenderWindow *window, int playerScore, int highScore, int numLives, int level, bool playerIsPlaying)
-{
-    window->draw(scoreBackground);
+    game->getWindow().draw(scoreBackground);
 
     if (playerIsPlaying) {
-        displayPlayerScore(window, playerScore);
-        displayHighScore(window, std::max(playerScore, highScore));
+        displayPlayerScore(game, playerScore);
+        displayHighScore(game, std::max(playerScore, highScore));
+        displayLevel(game, level);
+        displayNumLives(game, numLives);
     } else {
         text.setColor(sf::Color::White);
         text.setString("Press W to play");
         text.setPosition(15, 8);
-        window->draw(text);
+        game->getWindow().draw(text);
     }
-    
-    displayLevel(window, level);
-    displayNumLives(window, numLives);
 }
 
-void Hud::displayPlayerScore(sf::RenderWindow *window, int playerScore)
+void Hud::displayPlayerScore(GameEngine *game, int playerScore)
 {
     text.setColor(sf::Color::Green);
     text.setString(std::to_string(playerScore));
     text.setPosition(15, 8);
-    window->draw(text);
+    game->getWindow().draw(text);
 }
 
-void Hud::displayHighScore(sf::RenderWindow *window, int highScore)
+void Hud::displayHighScore(GameEngine *game, int highScore)
 {
     text.setColor(sf::Color::Red);
     text.setString(std::to_string(highScore));
-    text.setPosition(150, 8);
-    window->draw(text);
+    text.setPosition(game->getWindowWidth() / 3, 8);
+    game->getWindow().draw(text);
 }
 
-void Hud::displayLevel(sf::RenderWindow *window, int level)
+void Hud::displayLevel(GameEngine *game, int level)
 {
     text.setColor(sf::Color::White);
     text.setString("Level " + std::to_string(level));
-    text.setPosition(350, 8);
-    window->draw(text);
+    text.setPosition(game->getWindowWidth() - 110, 8);
+    game->getWindow().draw(text);
 }
 
-void Hud::displayNumLives(sf::RenderWindow *window, int numLives)
+void Hud::displayNumLives(GameEngine *game, int numLives)
 {
     for (int i{0}; i < numLives; i++) {
-        lives.setPosition((i * 43) + 50, 620 - 10);
-        window->draw(lives);
+        lives.setPosition((i * 43) + 50, game->getWindowHeight() - 10);
+        game->getWindow().draw(lives);
     }
 }
