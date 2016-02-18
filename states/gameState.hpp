b3dd1fad678e866a421d6718d3e1a3ee28b147d0
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-02-18 05:23:04 -0800

    New methods

--- states/gameState.hpp
+++ states/gameState.hpp
@@ -1,22 +1,30 @@
 #ifndef GAME_STATE_HPP
 #define GAME_STATE_HPP
 
-#include "..\stateman\gameEngine.hpp"
+//#include "..\stateman\gameEngine.hpp"
 
 class GameEngine;
 
-class GameState
-{
-public:
-    GameState();
-    ~GameState();
+//namespace Arkanoid
+//{
+    class GameState
+    {
+    public:
+        GameState();
+        ~GameState();
 
-    virtual void Pause() = 0;
-    virtual void Resume() = 0;
+        virtual void Init(GameEngine *game);
+        virtual void Cleanup();
 
-    virtual void HandleEvents(GameEngine *game) = 0;
-    virtual void Update(GameEngine *game) = 0;
-    virtual void Draw(GameEngine *game) = 0;
-};
+        virtual void Pause() = 0;
+        virtual void Resume() = 0;
+
+        virtual void HandleEvents() = 0;
+        virtual void Update() = 0;
+        virtual void Draw() = 0;
+        virtual void LoadResources() = 0;
+        virtual void LoadObjects() = 0;
+    };
+//}
 
 #endif // GAME_STATE_HPP
