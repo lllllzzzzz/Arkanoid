81e3a83cff65e1d201d0b33f36e373418b30f652
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-02-17 18:38:16 -0800

    Added functionality to Brick class

--- objects/brick.cpp
+++ objects/brick.cpp
@@ -1,41 +1,70 @@
 #include <ctime>
-
+#include <iostream>
 #include "Brick.hpp"
 
-Brick::Brick(float posX, float posY)
+const sf::Vector2f Brick::SIZE = {40, 20};
+const int Brick::SHADOW_OPACITY = 127;
+
+Brick::Brick(GameEngine *game, const sf::Vector2f position, const sf::Color colour, const int type) :
+    m_size(SIZE),
+    m_type(type),
+    m_isDestroyed(false),
+    m_isVisible(true)
 {
-    /*if (!texture.loadFromFile("data\\graphics\\brick_texture.png")) {
-    }
+    m_engine = game;
 
-    sprite.setTexture(texture);
+    shape.setPosition(position);
+    shape.setSize(SIZE);
+    shape.setFillColor(colour);
+    shape.setTexture(&m_engine->resourceMan.GetTexture("brick.png"));
+    shape.setOrigin(SIZE.x / 2.f, SIZE.y / 2.f);
 
-    sf::IntRect subRect;
-    subRect.left = 0;
-    subRect.top = 0;
-    subRect.width = 20;
-    subRect.height = 20;
-
-    shape.setTextureRect(subRect);
-
-    shape.setTexture(&texture);*/
-
-    shape.setPosition(posX, posY);
-    shape.setSize({brickWidth, brickHeight});
-
-    // Generate random colour for brick
-    //std::srand(std::time(0));
-
-    int r = std::rand() % 256;
-    int g = std::rand() % 256;
-    int b = std::rand() % 256;
-    sf::Color color(r, g, b);
-    shape.setFillColor(color);
-
-    shape.setOrigin(brickWidth / 2.f, brickHeight / 2.f);
-    isDestroyed = false;
+    shadow.setPosition({position.x + (m_size.x / 2), position.y + m_size.y});
+    shadow.setSize(SIZE);
+    shadow.setFillColor({0, 0, 0, SHADOW_OPACITY});
+    shadow.setOrigin(SIZE.x / 2.f, SIZE.y / 2.f);
 }
 
 Brick::~Brick()
 {
 
+}
+
+void Brick::Init(GameEngine *game)
+{
+    //texture = m_engine->resourceMan.GetTexture("brick.png");
+
+    //shape.setTexture(&texture);
+}
+
+void Brick::setPos(const sf::Vector2f newPosition)
+{
+    shape.setPosition(newPosition);
+    shadow.setPosition({newPosition.x + GetSize().x / 2, newPosition.y + GetSize().y});
+}
+
+void Brick::Draw()
+{
+    m_engine->getWindow().draw(shadow);
+    m_engine->getWindow().draw(shape);
+}
+
+const bool Brick::isVisible() const noexcept
+{
+    return m_isVisible;
+}
+
+void Brick::setVisibility(const bool isVisible) noexcept
+{
+    m_isVisible = isVisible;
+}
+
+bool Brick::isDestroyed() const noexcept
+{
+    return m_isDestroyed;
+}
+
+void Brick::destroy() noexcept
+{
+    m_isDestroyed = true;
 }
