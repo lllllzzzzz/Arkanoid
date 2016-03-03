f07f5fa9268b8a16d5c601952617d2ae118bc1f3
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-03-03 03:38:23 -0800

    Removed some comments

--- objects/shield.cpp
+++ objects/shield.cpp
@@ -1,20 +1,31 @@
 #include "shield.hpp"
 
-const int Shield::m_widthDefault = 430;
-const int Shield::m_heightDefault = 2;
+const int Shield::HEIGHT = 5;
+const int Shield::ELEVATION = 45;
+const sf::Color Shield::COLOUR = sf::Color::Green;
 
-Shield::Shield(const float x, const float y) :
-    m_width(m_widthDefault),
-    m_height(m_heightDefault),
-    m_isEnabled(true)
+Shield::Shield() :
+    m_isEnabled(false)
 {
-    shape.setPosition(x, y);
-    shape.setSize({m_width, m_height});
-    shape.setFillColor({255, 255, 255});
-    shape.setOrigin(0, 0);
+
 }
 
 Shield::~Shield()
 {
 
+}
+
+void Shield::Init(GameEngine *game)
+{
+    m_engine = game;
+
+    shape.setPosition(0, m_engine->getWindowSize().y - ELEVATION);
+    shape.setSize({m_engine->getWindowSize().x, HEIGHT});
+    shape.setFillColor(COLOUR);
+    shape.setOrigin(0, 0);
+}
+
+void Shield::Draw()
+{
+    m_engine->getWindow().draw(shape);
 }
