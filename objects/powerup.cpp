69b22c8d2e13fdd6e2b340c4fab769a789ac1de7
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-04-06 19:03:33 +1000

    Removed comment

--- objects/powerup.cpp
+++ objects/powerup.cpp
@@ -7,7 +7,6 @@ m_isDestroyed(false)
 {
     shape.setPosition(posX, posY);
     shape.setSize({width, height});
-    //shape.setFillColor(color);
     shape.setOrigin(width / 2.f, height / 2.f);
     shape.setTexture(&texture);
 }
