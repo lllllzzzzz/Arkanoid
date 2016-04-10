c2bdbfa76b82f482a30f8fd4c379643ebd176881
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-04-10 23:41:58 +1000

    Added constants

--- hud/hud.cpp
+++ hud/hud.cpp
@@ -1,8 +1,22 @@
 #include "hud.hpp"
 
-const sf::Color Hud::SCORE_BG_COLOUR = sf::Color::Black;
-const sf::Vector2f Hud::SCORE_BACKGROUND_SIZE = {480, 30};
-const int Hud::NUM_LIVES_ELEVATION = 10;
+#include <windows.h>
+
+const int Hud::SCORE_BACKGROUND_WIDTH = 480;
+const int Hud::SCORE_BACKGROUND_HEIGHT = 30;
+const int Hud::TEXT_SIZE = 12;
+const int Hud::LIVES_SPACING_X = 45;
+const int Hud::LIVES_OFFSET_X = 50;
+const int Hud::LIVES_ELEVATION = 10;
+const sf::Vector2f Hud::PLAYER_SCORE_POSITION = {15, 9};
+const sf::Vector2f Hud::HIGH_SCORE_POSITION = {100, 9};
+const sf::Vector2f Hud::LEVEL_POSITION = {110, 9};
+const sf::Vector2f Hud::PAUSED_POSITION = {250, 9};
+const sf::Vector2f Hud::LIVES_SIZE = {40, 10};
+const sf::Color Hud::SCORE_BACKGROUND_COLOUR = sf::Color::Black;
+const sf::Color Hud::PLAYER_SCORE_COLOUR = sf::Color::White;
+const sf::Color Hud::HIGH_SCORE_COLOUR = sf::Color::Red;
+const sf::Color Hud::LEVEL_COLOUR = sf::Color::White;
 
 Hud::Hud()
 {
@@ -19,62 +33,74 @@ void Hud::Init(GameEngine *game)
     m_engine = game;
 
     text.setFont(m_engine->resourceMan.GetFont("PressStart2P.ttf"));
-    text.setCharacterSize(14);
+    text.setCharacterSize(TEXT_SIZE);
 
-    lives.setSize({40, 10});
+    lives.setSize(LIVES_SIZE);
     lives.setTexture(&m_engine->resourceMan.GetTexture("paddle.png"));
-    lives.setOrigin(50 / 2, 20 / 2);
+    lives.setOrigin(LIVES_SIZE.x / 2, LIVES_SIZE.y / 2);
 
-    scoreBackground.setSize({m_engine->getWindowSize().x, SCORE_BACKGROUND_SIZE.y});
-    scoreBackground.setFillColor(sf::Color::Black);
+    scoreBackground.setSize({m_engine->getWindowSize().x, SCORE_BACKGROUND_HEIGHT});
+    scoreBackground.setFillColor(SCORE_BACKGROUND_COLOUR);
     scoreBackground.setOrigin(0, 0);
 }
 
-void Hud::DisplayHud(const int playerScore, const int highScore, const int numLives, const int level)
+void Hud::displayHud(const int playerScore, const int highScore, const int numLives, const int level, const bool gameIsRunning)
 {
     m_engine->getWindow().draw(scoreBackground);
 
     if (/*playerIsPlaying*/1) {
-        DisplayPlayerScore(playerScore);
-        DisplayHighScore(std::max(playerScore, highScore));
-        DisplayLevel(level);
-        DisplayNumLives(numLives);
+        displayPlayerScore(playerScore);
+        displayHighScore(std::max(playerScore, highScore));
+        displayLevel(level);
+        displayNumLives(numLives);
     } else {
         text.setColor(sf::Color::White);
         text.setString("Press W to play");
         text.setPosition(15, 8);
         m_engine->getWindow().draw(text);
     }
+
+    if (!gameIsRunning) {
+        displayPaused();
+    }
 }
 
-void Hud::DisplayPlayerScore(const int playerScore)
+void Hud::displayPlayerScore(const int playerScore)
 {
-    text.setColor(sf::Color::Green);
+    text.setColor(PLAYER_SCORE_COLOUR);
     text.setString(std::to_string(playerScore));
-    text.setPosition(15, 8);
+    text.setPosition(PLAYER_SCORE_POSITION);
     m_engine->getWindow().draw(text);
 }
 
-void Hud::DisplayHighScore(const int highScore)
+void Hud::displayHighScore(const int highScore)
 {
-    text.setColor(sf::Color::Red);
+    text.setColor(HIGH_SCORE_COLOUR);
     text.setString(std::to_string(highScore));
-    text.setPosition(m_engine->getWindowSize().x / 4, 8);
+    text.setPosition(HIGH_SCORE_POSITION);
     m_engine->getWindow().draw(text);
 }
 
-void Hud::DisplayLevel(const int level)
+void Hud::displayLevel(const int level)
 {
-    text.setColor(sf::Color::White);
+    text.setColor(LEVEL_COLOUR);
     text.setString("Level " + std::to_string(level));
-    text.setPosition(m_engine->getWindowSize().x - 110, 8);
+    text.setPosition(m_engine->getWindowSize().x - LEVEL_POSITION.x, LEVEL_POSITION.y);
     m_engine->getWindow().draw(text);
 }
 
-void Hud::DisplayNumLives(const int numLives)
+void Hud::displayNumLives(const int numLives)
 {
     for (int i{0}; i < numLives; i++) {
-        lives.setPosition((i * 43) + 50, m_engine->getWindowSize().y - NUM_LIVES_ELEVATION);
+        lives.setPosition((i * LIVES_SPACING_X) + LIVES_OFFSET_X, m_engine->getWindowSize().y - LIVES_ELEVATION);
         m_engine->getWindow().draw(lives);
     }
+}
+
+void Hud::displayPaused()
+{
+    text.setColor(sf::Color::White);
+    text.setString("Paused");
+    text.setPosition(m_engine->getWindowSize().x - PAUSED_POSITION.x, PAUSED_POSITION.y);
+    m_engine->getWindow().draw(text);
 }
