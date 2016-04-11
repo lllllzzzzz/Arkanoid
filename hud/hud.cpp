bcf12ccf8cd0afd6f75dc1405af0c709a9d3fc75
Luke Zimmerer <lukezimmerer@users.noreply.github.com>
2016-04-11 20:57:57 +1000

    Added game over message box

--- hud/hud.cpp
+++ hud/hud.cpp
@@ -4,16 +4,20 @@
 
 const int Hud::SCORE_BACKGROUND_WIDTH = 480;
 const int Hud::SCORE_BACKGROUND_HEIGHT = 30;
+const sf::Vector2f Hud::WIN_LOSE_BACKGROUND_SIZE = {120, 60};
+const sf::Vector2f Hud::WIN_LOSE_BACKGROUND_BORDER_SIZE = {126, 66};
 const int Hud::TEXT_SIZE = 12;
 const int Hud::LIVES_SPACING_X = 45;
 const int Hud::LIVES_OFFSET_X = 50;
 const int Hud::LIVES_ELEVATION = 10;
 const sf::Vector2f Hud::PLAYER_SCORE_POSITION = {15, 9};
 const sf::Vector2f Hud::HIGH_SCORE_POSITION = {100, 9};
-const sf::Vector2f Hud::LEVEL_POSITION = {110, 9};
-const sf::Vector2f Hud::PAUSED_POSITION = {250, 9};
+const sf::Vector2f Hud::LEVEL_POSITION = {100, 9};
+const sf::Vector2f Hud::PAUSED_POSITION = {210, 9};
 const sf::Vector2f Hud::LIVES_SIZE = {40, 10};
 const sf::Color Hud::SCORE_BACKGROUND_COLOUR = sf::Color::Black;
+const sf::Color Hud::WIN_LOSE_BACKGROUND_COLOUR = sf::Color::Black;
+const sf::Color Hud::WIN_LOSE_BACKGROUND_BORDER_COLOUR = sf::Color(127, 127, 127);
 const sf::Color Hud::PLAYER_SCORE_COLOUR = sf::Color::White;
 const sf::Color Hud::HIGH_SCORE_COLOUR = sf::Color::Red;
 const sf::Color Hud::LEVEL_COLOUR = sf::Color::White;
@@ -42,26 +46,30 @@ void Hud::Init(GameEngine *game)
     scoreBackground.setSize({m_engine->getWindowSize().x, SCORE_BACKGROUND_HEIGHT});
     scoreBackground.setFillColor(SCORE_BACKGROUND_COLOUR);
     scoreBackground.setOrigin(0, 0);
+
+    winLoseBackground.setSize(WIN_LOSE_BACKGROUND_SIZE);
+    winLoseBackground.setFillColor(WIN_LOSE_BACKGROUND_COLOUR);
+    winLoseBackground.setOrigin(0, 0);
+    winLoseBackground.setPosition(m_engine->getWindowSize().x / 2 - 65, m_engine->getWindowSize().y / 2 - 25);
+
+    winLoseBackgroundBorder.setSize(WIN_LOSE_BACKGROUND_BORDER_SIZE);
+    winLoseBackgroundBorder.setFillColor(WIN_LOSE_BACKGROUND_BORDER_COLOUR);
+    winLoseBackgroundBorder.setOrigin(0, 0);
+    winLoseBackgroundBorder.setPosition(m_engine->getWindowSize().x / 2 - 68, m_engine->getWindowSize().y / 2 - 28);
 }
 
 void Hud::displayHud(const int playerScore, const int highScore, const int numLives, const int level, const bool gameIsRunning)
 {
     m_engine->getWindow().draw(scoreBackground);
 
-    if (/*playerIsPlaying*/1) {
-        displayPlayerScore(playerScore);
-        displayHighScore(std::max(playerScore, highScore));
-        displayLevel(level);
-        displayNumLives(numLives);
-    } else {
-        text.setColor(sf::Color::White);
-        text.setString("Press W to play");
-        text.setPosition(15, 8);
-        m_engine->getWindow().draw(text);
-    }
+    displayPlayerScore(playerScore);
+    displayHighScore(std::max(playerScore, highScore));
+    displayLevel(level);
+    displayNumLives(numLives);
 
     if (!gameIsRunning) {
         displayPaused();
+        //displayWinLose(true);
     }
 }
 
@@ -102,5 +110,18 @@ void Hud::displayPaused()
     text.setColor(sf::Color::White);
     text.setString("Paused");
     text.setPosition(m_engine->getWindowSize().x - PAUSED_POSITION.x, PAUSED_POSITION.y);
+    m_engine->getWindow().draw(text);
+}
+
+void Hud::displayWinLose(bool playerWon)
+{
+    m_engine->getWindow().draw(winLoseBackgroundBorder);
+    m_engine->getWindow().draw(winLoseBackground);
+
+    text.setColor(sf::Color::White);
+
+    text.setString(playerWon ? "You win!" : "You lose!");
+
+    text.setPosition(m_engine->getWindowSize().x / 2 - 50, m_engine->getWindowSize().y / 2);
     m_engine->getWindow().draw(text);
 }
