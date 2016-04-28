9e697fdd4400dab89e93ff3b529afa39ebae1132
U-Luke-PC\Luke <lukez76@hotmail.com>
2016-04-28 18:08:52 +1000

    Commiting local copy of files

--- player/player.cpp
+++ player/player.cpp
@@ -3,8 +3,8 @@
 const int Player::DEFAULT_LIVES = 2;
 
 Player::Player() :
-    m_points(0),
-    m_lives(0)
+	m_points(0),
+	m_lives(0)
 {
 
 }
@@ -16,13 +16,14 @@ Player::~Player()
 
 void Player::Init(GameEngine *game)
 {
-    m_engine = game;
-    m_paddle.Init(m_engine);
+	m_engine = game;
+
+	m_paddle.Init(m_engine);
 }
 
 void Player::Reset()
 {
-    m_paddle.Reset();
-    SetPoints(0);
-    SetLives(DEFAULT_LIVES);
+	m_paddle.Reset();
+	SetPoints(0);
+	SetLives(DEFAULT_LIVES);
 }
